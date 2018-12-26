95b7552fc24ad8518d59347c5acdccb918c027fb
chenyulong <chenyulong@DESKTOP-MC561GD>
2018-12-26 16:40:03 +0800

    修改医生请假

--- 医院预约系统/doctor/UCVacation.cs
+++ 医院预约系统/doctor/UCVacation.cs
@@ -26,23 +26,24 @@ namespace Hospital
         {
             string leftStartTime ="";
             string leftEndTime = "";
-            DateTime now = new DateTime(2018,1,1);
+            DateTime now = DateTime.Today;
             string reason = textBox1.Text;
            // now = DateTime.Now;
-            if ((dateTimePicker1.Value < now)||(dateTimePicker2.Value < dateTimePicker1.Value))
-            {
-                MessageBox.Show("请输入正确的请假时间！");
-                dateTimePicker1.Value = now;
-                dateTimePicker2.Value=dateTimePicker1.Value;
-            }
-            else
+            if ((dateTimePicker1.Value.Date >= now)&&(dateTimePicker2.Value.Date >= dateTimePicker1.Value.Date))
             {
                 leftStartTime = dateTimePicker1.Value.Year.ToString() + "-" + dateTimePicker1.Value.Month.ToString() + "-" + dateTimePicker1.Value.Day.ToString();
                 leftEndTime = dateTimePicker2.Value.Year.ToString() + "-" + dateTimePicker2.Value.Month.ToString() + "-" + dateTimePicker2.Value.Day.ToString();
-                vacationinfo vctinfo = new vacationinfo(id,name,leftStartTime,leftEndTime,reason);
+                vacationinfo vctinfo = new vacationinfo(id, name, leftStartTime, leftEndTime, reason);
                 vctinfo.ShowDialog();
             }
-               
+            else
+            {
+                MessageBox.Show("请输入正确的请假时间！");
+               dateTimePicker1.Value = now;
+                dateTimePicker2.Value = dateTimePicker1.Value;
+
+            }
+
         }
 
     }
